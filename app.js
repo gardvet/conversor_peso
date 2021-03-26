9c8e91c8853a54e45abf6de9f0017ecae68c2206
Hildegard <48067323+gardvet@users.noreply.github.com>
2021-03-26 12:27:01 -0300

    upload files
    projeto em andamento

---
+++ app.js
@@ -0,0 +1,7 @@
+let grama = document.getElementById('grama');
+
+function conversor(grama) {
+    document.getElementById('grama').innerHTML = (document.getElementById('result').value/1000 + Kg);
+}; 
+
+
